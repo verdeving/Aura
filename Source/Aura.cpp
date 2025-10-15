a68cc76b7e27f140aaa017a9c41198917389370a
verodeving <verodeving@gmail.com>
2025-10-15 16:17:14 +0200

    Added Aura

---
+++ Source/Aura.cpp
@@ -0,0 +1,11 @@
+#if aura_major >= 0 and aura_middle >= 0 and aura_minor >= 1
+#ifdef aura_windows
+#include <Windows.h>
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+    return 0;
+}
+
+#endif
+#endif
